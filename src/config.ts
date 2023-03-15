89b3a3c658a5c888bfb7918a2cfe4ccf3b4df0e0
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-03-15 09:46:06 -0300

    first commit

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const apiEndpoint =
+  "https://y190ll3uhh.execute-api.us-east-1.amazonaws.com/dev";
+
+export const authConfig = {
+  domain: "...",
+  clientId: "...",
+  callbackUrl: "http://localhost:3000/callback",
+};
