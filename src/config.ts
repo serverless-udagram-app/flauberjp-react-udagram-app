edf15e31b9897739b88d543c0d4533e7faff509d
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-04-01 11:57:41 -0300

    updating api and oauth info

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const apiEndpoint =
-  "https://y190ll3uhh.execute-api.us-east-1.amazonaws.com/dev";
+  "https://7xkvufpzpk.execute-api.us-east-1.amazonaws.com/dev";
 
 export const authConfig = {
-  domain: "...",
-  clientId: "...",
+  domain: "dev-gi7tpkcun11ksfq7.us.auth0.com",
+  clientId: "CRnaTvJUoVSlDwWnyVWQcEQvMPP2jFpc",
   callbackUrl: "http://localhost:3000/callback",
 };
